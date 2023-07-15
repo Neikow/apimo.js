c801fb990c139f85a348120c39ab2cff3a6f70ef
Vitaly <50383391+Neikow@users.noreply.github.com>
2023-07-15 10:23:51 +0200

    :sparkles: Full rewrite using `zod`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from "./core";
+export {default, ApimoSettings, PathPart, Agency, Property, Catalog} from "./core";
 export * from "./schemas/agency";
 export * from "./schemas/common";
 export * from "./schemas/property";
